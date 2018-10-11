31cc4b36c55bd47708ba5b44ed007a187bfbdead
Jose Manuel <josman_more23@hotmail.com>
2018-10-11 13:32:15 -0500

    Implementación de ejercicio static.

--- src/ejemplostatic/Galleta.java
+++ src/ejemplostatic/Galleta.java
@@ -1,4 +1,66 @@
 package ejemplostatic;
 
 public class Galleta {
+
+    private String sabor;
+    private String forma;
+    private String color;
+    private String relleno;
+    private static int cantidad=0;
+
+    public Galleta(){
+    }
+
+    public Galleta(String sabor, String forma, String color, String relleno) {
+        this.sabor = sabor;
+        this.forma = forma;
+        this.color = color;
+        this.relleno = relleno;
+        this.cantidad ++;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRelleno() {
+        return relleno;
+    }
+
+    public void setRelleno(String relleno) {
+        this.relleno = relleno;
+    }
+
+    public static int getCantidad() {
+        return cantidad;
+    }
+
+    public static void setCantidad(int cantidad) {
+        Galleta.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return this.sabor+" -- "+this.color+" -- "+this.forma+" -- "+this.relleno;
+    }
 }
--- src/ejemplostatic/MainGalletas.java
+++ src/ejemplostatic/MainGalletas.java
@@ -1,4 +1,57 @@
 package ejemplostatic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainGalletas {
+
+    public static void main(String[] args){
+        List<Galleta> galletas = misgalletas();
+
+        for(Galleta gall : galletas){
+            System.out.println(gall.toString());
+        }
+
+        System.out.println(galletas.get(0).getCantidad());
+
+    }
+
+    public static List<Galleta> misgalletas(){
+
+        Galleta galleta1= new Galleta("Chocolate","Cuadrada","Café","Cremoso");
+        Galleta galleta2= new Galleta("Vainilla","Cuadrada","Amarilla","Cremoso");
+        Galleta galleta3= new Galleta("Fresa","Cuadrada","Rojo","Cremoso");
+        Galleta galleta4= new Galleta("Chocolate","Redonda","Café","Cremoso");
+        Galleta galleta5= new Galleta("Vainilla","Redonda","Amarilla","Cremoso");
+        Galleta galleta6= new Galleta("Fresa","Redonda","","Roja");
+        Galleta galleta7= new Galleta("Chocolate","Rectangular","Café","Cremoso");
+        Galleta galleta8= new Galleta("Vainilla","Rectangular","Amarilla","Cremoso");
+        Galleta galleta9= new Galleta("Fresa","Rectangular","Roja","Cremoso");
+        Galleta galleta10= new Galleta("Chocolate","Animalito","Café","Cremoso");
+        Galleta galleta11= new Galleta("Vainilla","Animalito","Amarilla","Cremoso");
+        Galleta galleta12= new Galleta("Coco","Animalito","Blanca","Cremoso");
+        Galleta galleta13= new Galleta("Chocolate","Ondulada","Café","Cremoso");
+        Galleta galleta14= new Galleta("Vainilla","Ondulada","Amarilla","Cremoso");
+        Galleta galleta15= new Galleta("Chocolate","Ondulada","Café","Cremoso");
+
+        List<Galleta> galleta = new ArrayList<>();
+        galleta.add(galleta1);
+        galleta.add(galleta2);
+        galleta.add(galleta3);
+        galleta.add(galleta4);
+        galleta.add(galleta5);
+        galleta.add(galleta6);
+        galleta.add(galleta7);
+        galleta.add(galleta8);
+        galleta.add(galleta9);
+        galleta.add(galleta10);
+        galleta.add(galleta11);
+        galleta.add(galleta12);
+        galleta.add(galleta13);
+        galleta.add(galleta14);
+        galleta.add(galleta15);
+
+        return galleta;
+
+    }
 }
