215f2b422d1e3756fe2ffca50fc7e85a4188e160
Jose Manuel <josman_more23@hotmail.com>
2018-10-17 09:48:23 -0500

    Ejercicio de operador ternario y hacer una validación sustituyendo if

--- src/operadorTernario/TestTernario.java
+++ src/operadorTernario/TestTernario.java
@@ -1,4 +1,16 @@
 package operadorTernario;
 
 public class TestTernario {
+
+    public static void main(String[] args){
+        ValidarAcceso(13,19,17,21,35,15);
+    }
+
+   public static void ValidarAcceso(int... valores){
+
+        for(int val: valores){
+            String status = (val >= 18) ? "Tu edad es: "+val+", Acceso permitido" : "Tu edad es: "+val+", Acceso denegado";
+            System.out.println(status);
+        }
+    }
 }
